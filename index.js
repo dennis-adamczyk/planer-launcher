bc4384a342c9def98687b417ee051b7691e1c2fe
Dennis Adamczyk <ich@dennis-adamczyk.de>
2020-12-21 14:39:28 +0100

    Test webhook 6

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const puppeteer = require('puppeteer');
 
   const page = await browser.newPage();
   await page.goto('http://192.168.178.31:8080/');
-  // Test 5
+  // Test 6
 
   await browser.close();
 })();
