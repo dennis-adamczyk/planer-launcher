f607a806df291c656ea73a3a1fa51ca6d084c2d3
Dennis Adamczyk <ich@dennis-adamczyk.de>
2020-12-21 01:05:35 +0100

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    // args: ['--start-fullscreen'],
+    ignoreDefaultArgs: ['--enable-automation'],
+  });
+  const defaultPage = (await browser.pages())[0];
+  defaultPage.close();
+  const page = await browser.newPage();
+  await page.goto('http://192.168.178.31:8080/');
+
+  // await browser.close();
+})();
