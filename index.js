22269abcd6cb08c28efa1d8a0e09baf828c8b1c5
Dennis Adamczyk <ich@dennis-adamczyk.de>
2020-12-22 01:16:30 +0100

    Add application mode

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    args: ['--start-fullscreen', '--kiosk'],
+    args: ['--start-fullscreen', '--app'],
     ignoreDefaultArgs: ['--enable-automation'],
     executablePath: '/usr/bin/chromium-browser',
   });
